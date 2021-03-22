5f6ccace47ef5f1434dffcdf2704a10018429d42
Goggiafk <58605218+Goggiafk@users.noreply.github.com>
2021-03-22 16:14:11 +0100

    Initial commit

---
+++ Assets/Scripts/movementScript.cs
@@ -0,0 +1,58 @@
+﻿using SimpleInputNamespace;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movementScript : MonoBehaviour
+{
+    public GameObject platform;
+    public GameObject deathScreen;
+    public Joystick joystick;
+    // Start is called before the first frame update
+
+    public CharacterController2D controller;
+
+    public static float playerSpeed = 40f;
+
+    float horizontalMove = 0f;
+
+    bool jump = false;
+    bool crouch = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = joystick.xAxis.value * playerSpeed;
+    }
+    
+    public void Crouch()
+    {
+        crouch = !crouch;
+    }
+    public void Jump()
+    {
+        jump = true;
+        platform.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+
+    }
+    
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.transform.tag == "Fan")
+        {
+            Debug.Log("fff");
+            Time.timeScale = 0;
+            deathScreen.SetActive(true);
+        }
+    }
+}
